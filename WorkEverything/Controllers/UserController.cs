8a91a5a60c94d47e4e7a6b6c3d62b1694dd387e1
Loc Phan <loc@gmail.com>
2017-11-30 21:47:43 +0700

    Add loc

---
+++ WorkEverything/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WorkEverything.Database;
+using WorkEverything.ViewModel.User;
+
+namespace WorkEverything.Controllers
+{
+    public class UserController : Controller
+    {
+        DatabaseModel database = new DatabaseModel();
+        //
+        // GET: /User/
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RequestLogin(UserModel model) 
+        {
+            if (ModelState.IsValid)
+            {
+                //var passWord = database.TB_User.Where(x=>x.UserName == model.userName).FirstOrDefault().PassWord;
+                var passWord = database.Database.SqlQuery<TB_User>("SELECT * FROM TB_User WHERE UserName='" + model.userName + "'").FirstOrDefault();
+                if (passWord.PassWord == model.passWord)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Đăng nhập không đúng");
+                    return View(model);
+                }
+            }
+            else
+            {
+                return View(model);
+            }
+            
+        }
+	}
+	
+}
